beeef41b8cbb5c75bbcd33165a902299e900d1e0
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-04-16 13:43:28 -0600

    Excersices 4, 5 and 6 of class 2

--- 2-Principales metodos del objeto string/Ejercicios/Ejercicios metodos del objeto string.py
+++ 2-Principales metodos del objeto string/Ejercicios/Ejercicios metodos del objeto string.py
@@ -36,3 +36,41 @@ print('\"Python 3\", son solo caracteres alfabeticos? -', 'Python 3'.isalpha()) 
 print('\"Python3\", son solo caracteres alfabeticos? -', 'Python3'.isalpha())
 print('\"Python3.8\", son solo caracteres alfabeticos? -', 'Python3.8'.isalpha())
 print('\"Python\", son solo caracteres alfabeticos? -', 'Python'.isalpha()) # Evaluando sin numero
+print('_______________________________________________________')
+
+
+# Ejercicio Numero 4
+'''Utilizando métodos de cadenas, identificar cuál de los siguientes caracteres 
+son ascii: ñ, d, s, a, $, &.'''
+print('Ejercicio Numero 4')
+print('\"ñ\" es ascci? - ', 'ñ'.isascii()) # Evaluando caracteres
+print('\"d\" es ascci? - ', 'd'.isascii())
+print('\"s\" es ascci? - ', 's'.isascii())
+print('\"á\" es ascci? - ', 'á'.isascii())
+print('\"$\" es ascci? - ', '$'.isascii())
+print('\"&\" es ascci? - ', '&'.isascii())
+print('\"~\" es ascci? - ', '~'.isascii())
+print('_______________________________________________________')
+
+
+# Ejercicio numero 5
+'''Utilizando métodos de cadenas, crear una variable con una lista de números 
+pares del 2 al 10 como cadenas e imprimir sus valores separados por un guión medio.'''
+print('Ejercicio Numero 5')
+pares = ['2', '4', '6', '8', '10', '12'] # Tienen que ser tipo string para que funcione
+print('Numeros pares =', '|'.join(pares)) # Imprimiendo pares con '|' en medio
+impares = ['1', '3', '5', '7', '9', '11']
+print('Numeros impares =', '|'.join(impares))
+print('_______________________________________________________')
+
+
+# Ejercicio Numero 6
+'''Del ejercicio anterior, guardar en una variable el valor impreso y 
+volver a separar sus valores utilizando métodos de cadenas.'''
+print('Ejercicio Numero 6')
+impresion_pares = '|'.join(pares) # Almacenando la impresion anterior con join()
+print('Ejercicio anterior con pares: ', impresion_pares)
+impresion_impares = '|'.join(impares)
+print('Ejercicio anterior con impares: ', impresion_impares)
+print('Obteniendo lista original de pares: ', impresion_pares.split('|')) # Obteniendo la lista original partiendo de la impresion almacenada
+print('Obteniendo lista original de impares: ', impresion_impares.split('|'))
