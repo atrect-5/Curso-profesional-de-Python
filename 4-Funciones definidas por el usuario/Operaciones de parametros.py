a918e85b702a9bc4818cd8d03672c89ce2d14790
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-04-22 14:58:12 -0600

    Class 3 from module 4 done

---
+++ 4-Funciones definidas por el usuario/Operaciones de parametros.py
@@ -0,0 +1,9 @@
+
+
+# Creamos una funcion que no hace nada, para ver como podemos documentarla para ver esos datos despues
+def Function():
+    """Documentando la funcion: Esta funcion no hace nada"""
+    pass
+
+print(Function.__doc__) # Accediendo al atributo "__doc__" podemos ver la documentacion de nuestra funcion
+# Documentando la funcion: Esta funcion no hace nada
