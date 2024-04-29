3b8c2d6902c05139353c6383beb64c520da15860
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-04-29 12:04:25 -0600

    Impresion base finished

--- 6-POO/Ejercicios/1-Cuadrante.py
+++ 6-POO/Ejercicios/1-Cuadrante.py
@@ -10,21 +10,38 @@ class Punto:
         self.y = y
 
     def ubicacion(self): # Definimos la funcion que ubicara los puntos en el plano
-        if (self.x >= 0): # Si x es positivo, se ubicara del lado derecho del plano.
-            if (self.y >= 0): # Si y es positivo, se ubicara del lado superior del plano
+        if (self.x > 0): # Si 'x' es positivo, se ubicara del lado derecho del plano.
+            if (self.y > 0): # Si 'y' es positivo, se ubicara del lado superior del plano
                 print('Cuadrante superior derecho (+,+)')
                 print('  | 0\n--+--\n  |  ')
-            else:
+            elif (self.y < 0): # Si 'y' es negativo, se ubicara del lado inferior del plano
                 print('Cuadrante inferior derecho (+,-)')
                 print('  |  \n--+--\n  | 0')
-        
-        else:
-            if (self.y >= 0):
+            else : # Si 'y' no es mayor ni menor que 0 (Es 0), se ubicara el en Eje x
+                print('Eje \'x\' positivo (+,0)')
+                print('  |  \n--+-0\n  |')
+
+        elif (self.x < 0): # Si 'x' es negativo, se ubicara del lado izquierdo del plano.
+            if (self.y > 0):
                 print('Cuadrante superior izquierdo (-,+)')
                 print('0 |  \n--+--\n  |  ')
-            else:
+            elif (self.y < 0):
                 print('Cuadrante inferior izquierdo (-,-)')
                 print('  |  \n--+--\n0 |  ')
+            else:
+                print('Eje \'x\' negativo (-,0)')
+                print('  |  \n0-+--\n  |')
+
+        else: # Si 'x' no es mayor ni menor que 0 (Es 0), se ubicara en el Eje y
+            if (self.y > 0):
+                print('Eje \'y\' superior (0,+)')
+                print('  0  \n--+--\n  |  ')
+            elif (self.y < 0):
+                print('Eje \'y\' inferior (0,-)')
+                print('  |  \n--+--\n  0  ')
+            else:
+                print('Origen (0,0)')
+                print('  |  \n--0--\n  |')
 
 # Recibimos los valores de la coordenadas por medio de 'input' y los asignamos a los atributos 'x' y 'y'
 p = Punto(float(input('Ingresa la coordenada del eje x : ')),float(input('Ingresa la coordenada del eje y : ')))
--- 6-POO/Ejercicios/_2_Cuadrante_property.py
+++ 6-POO/Ejercicios/_2_Cuadrante_property.py
@@ -7,26 +7,38 @@ class Punto:
     def __init__(self, x, y) -> None: 
         self.x = x
         self.y = y
-
     @property # Hacemos de la funcion un @property para poder acceder a esta como un atributo y no como un metodo
     def ubicacion(self): 
-        print('Coordenada X : ', self.x, '| Coordenada Y : ', self.y)
-        if (self.x >= 0): 
-            if (self.y >= 0):
+        if (self.x > 0):
+            if (self.y > 0):
                 print('Cuadrante superior derecho (+,+)')
                 print('  | 0\n--+--\n  |  ')
-            else:
+            elif (self.y < 0):
                 print('Cuadrante inferior derecho (+,-)')
                 print('  |  \n--+--\n  | 0')
-        
-        else:
-            if (self.y >= 0):
+            else :
+                print('Eje \'x\' positivo (+,0)')
+                print('  |  \n--+-0\n  |')
+        elif (self.x < 0): 
+            if (self.y > 0):
                 print('Cuadrante superior izquierdo (-,+)')
                 print('0 |  \n--+--\n  |  ')
-            else:
+            elif (self.y < 0):
                 print('Cuadrante inferior izquierdo (-,-)')
                 print('  |  \n--+--\n0 |  ')
-
+            else:
+                print('Eje \'x\' negativo (-,0)')
+                print('  |  \n0-+--\n  |')
+        else: 
+            if (self.y > 0):
+                print('Eje \'y\' superior (0,+)')
+                print('  0  \n--+--\n  |  ')
+            elif (self.y < 0):
+                print('Eje \'y\' inferior (0,-)')
+                print('  |  \n--+--\n  0  ')
+            else:
+                print('Origen (0,0)')
+                print('  |  \n--0--\n  |')
 if __name__ == '__main__':
     # Recibimos los valores de la coordenadas por medio de 'input' y los asignamos a los atributos 'x' y 'y'
     p = Punto(float(input('Ingresa la coordenada del eje x : ')),float(input('Ingresa la coordenada del eje y : ')))
