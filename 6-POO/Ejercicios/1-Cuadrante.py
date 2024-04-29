85b0d236ee8a68e6baea543033ae63c7c6b505f5
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-04-29 12:41:46 -0600

    printing plane

--- 6-POO/Ejercicios/1-Cuadrante.py
+++ 6-POO/Ejercicios/1-Cuadrante.py
@@ -13,36 +13,72 @@ class Punto:
         if (self.x > 0): # Si 'x' es positivo, se ubicara del lado derecho del plano.
             if (self.y > 0): # Si 'y' es positivo, se ubicara del lado superior del plano
                 print('Cuadrante superior derecho (+,+)')
-                print('  | 0\n--+--\n  |  ')
+                for h in reversed(range(self.y +1 )):
+                    if (h==self.y-1):
+                        wigth=' |'+' '*(self.x-1)+'0'
+                        print(wigth)
+                    else:
+                        print(' |')
+                wigth = '—|'+'—'*(self.x)
+                print(wigth)
             elif (self.y < 0): # Si 'y' es negativo, se ubicara del lado inferior del plano
                 print('Cuadrante inferior derecho (+,-)')
-                print('  |  \n--+--\n  | 0')
+                wigth = '—|'+'—'*(self.x)
+                print(wigth)
+                for h in range(self.y*-1 +1 ):
+                    if (h==self.y*-1-1):
+                        wigth=' |'+' '*(self.x-1)+'0'
+                        print(wigth)
+                    else:
+                        print(' |')
             else : # Si 'y' no es mayor ni menor que 0 (Es 0), se ubicara el en Eje x
                 print('Eje \'x\' positivo (+,0)')
-                print('  |  \n--+-0\n  |')
+                wigth = '—|'+'—'*(self.x-1)+'0'
+                print(wigth)
 
         elif (self.x < 0): # Si 'x' es negativo, se ubicara del lado izquierdo del plano.
             if (self.y > 0):
                 print('Cuadrante superior izquierdo (-,+)')
-                print('0 |  \n--+--\n  |  ')
+                for h in reversed(range(self.y +1 )):
+                    if (h==self.y-1):
+                        wigth='0'+' '*(self.x*-1-1)+'|'
+                        print(wigth)
+                    else:
+                        print(" "*(self.x*(-1))+'|')
+                wigth = '—'*(self.x*-1)+'|—'
+                print(wigth)
             elif (self.y < 0):
                 print('Cuadrante inferior izquierdo (-,-)')
-                print('  |  \n--+--\n0 |  ')
+                wigth = '—'*(self.x*-1)+'|—'
+                print(wigth)
+                for h in range(self.y*-1+1 ):
+                    if (h==self.y*-1-1):
+                        wigth='0'+' '*(self.x*-1-1)+'|'
+                        print(wigth)
+                    else:
+                        print(" "*(self.x*(-1))+'|')
             else:
                 print('Eje \'x\' negativo (-,0)')
-                print('  |  \n0-+--\n  |')
+                wigth = '0'+'—'*(self.x*-1-1)+'|—'
+                print(wigth)
 
         else: # Si 'x' no es mayor ni menor que 0 (Es 0), se ubicara en el Eje y
             if (self.y > 0):
                 print('Eje \'y\' superior (0,+)')
-                print('  0  \n--+--\n  |  ')
+                print(' 0')
+                for h in range(self.y-1):
+                    print(' |')
+                print('—|—')
             elif (self.y < 0):
                 print('Eje \'y\' inferior (0,-)')
-                print('  |  \n--+--\n  0  ')
+                print('—|—')
+                for h in range(self.y*-1-1):
+                    print(' |')
+                print(' 0')
             else:
                 print('Origen (0,0)')
                 print('  |  \n--0--\n  |')
 
 # Recibimos los valores de la coordenadas por medio de 'input' y los asignamos a los atributos 'x' y 'y'
-p = Punto(float(input('Ingresa la coordenada del eje x : ')),float(input('Ingresa la coordenada del eje y : ')))
+p = Punto(int(input('Ingresa la coordenada del eje x : ')),int(input('Ingresa la coordenada del eje y : ')))
 p.ubicacion()
--- 6-POO/Ejercicios/_2_Cuadrante_property.py
+++ 6-POO/Ejercicios/_2_Cuadrante_property.py
@@ -9,37 +9,77 @@ class Punto:
         self.y = y
     @property # Hacemos de la funcion un @property para poder acceder a esta como un atributo y no como un metodo
     def ubicacion(self): 
+        wigth = ''
         if (self.x > 0):
             if (self.y > 0):
                 print('Cuadrante superior derecho (+,+)')
-                print('  | 0\n--+--\n  |  ')
+                for h in reversed(range(self.y +1 )):
+                    if (h==self.y-1):
+                        wigth=' |'+' '*(self.x-1)+'0'
+                        print(wigth)
+                    else:
+                        print(' |')
+                wigth = '—|'+'—'*(self.x)
+                print(wigth)
+
             elif (self.y < 0):
                 print('Cuadrante inferior derecho (+,-)')
-                print('  |  \n--+--\n  | 0')
+                wigth = '—|'+'—'*(self.x)
+                print(wigth)
+                for h in range(self.y*-1 +1 ):
+                    if (h==self.y*-1-1):
+                        wigth=' |'+' '*(self.x-1)+'0'
+                        print(wigth)
+                    else:
+                        print(' |')
             else :
                 print('Eje \'x\' positivo (+,0)')
-                print('  |  \n--+-0\n  |')
+                wigth = '—|'+'—'*(self.x-1)+'0'
+                print(wigth)
+
         elif (self.x < 0): 
             if (self.y > 0):
                 print('Cuadrante superior izquierdo (-,+)')
-                print('0 |  \n--+--\n  |  ')
+                for h in reversed(range(self.y +1 )):
+                    if (h==self.y-1):
+                        wigth='0'+' '*(self.x*-1-1)+'|'
+                        print(wigth)
+                    else:
+                        print(" "*(self.x*(-1))+'|')
+                wigth = '—'*(self.x*-1)+'|—'
+                print(wigth)
             elif (self.y < 0):
                 print('Cuadrante inferior izquierdo (-,-)')
-                print('  |  \n--+--\n0 |  ')
+                wigth = '—'*(self.x*-1)+'|—'
+                print(wigth)
+                for h in range(self.y*-1+1 ):
+                    if (h==self.y*-1-1):
+                        wigth='0'+' '*(self.x*-1-1)+'|'
+                        print(wigth)
+                    else:
+                        print(" "*(self.x*(-1))+'|')
+                
             else:
                 print('Eje \'x\' negativo (-,0)')
-                print('  |  \n0-+--\n  |')
+                wigth = '0'+'—'*(self.x*-1-1)+'|—'
+                print(wigth)
         else: 
             if (self.y > 0):
                 print('Eje \'y\' superior (0,+)')
-                print('  0  \n--+--\n  |  ')
+                print(' 0')
+                for h in range(self.y-1):
+                    print(' |')
+                print('—|—')
             elif (self.y < 0):
                 print('Eje \'y\' inferior (0,-)')
-                print('  |  \n--+--\n  0  ')
+                print('—|—')
+                for h in range(self.y*-1-1):
+                    print(' |')
+                print(' 0')
             else:
                 print('Origen (0,0)')
                 print('  |  \n--0--\n  |')
 if __name__ == '__main__':
     # Recibimos los valores de la coordenadas por medio de 'input' y los asignamos a los atributos 'x' y 'y'
-    p = Punto(float(input('Ingresa la coordenada del eje x : ')),float(input('Ingresa la coordenada del eje y : ')))
+    p = Punto(int(input('Ingresa la coordenada del eje x : ')),int(input('Ingresa la coordenada del eje y : ')))
     p.ubicacion # Podemos apreciar que al acceder al metodo 'ubicacio()', lo hacemos como si se tratase de un atributo
