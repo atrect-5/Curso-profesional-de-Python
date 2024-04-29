9e3b4e06511f8a9033801c6e6cbb5a30807f4a6c
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-04-29 13:15:05 -0600

    Add a comment Cuadrante.py

--- 6-POO/Ejercicios/1-Cuadrante.py
+++ 6-POO/Ejercicios/1-Cuadrante.py
@@ -1,6 +1,5 @@
 # Alejandro Gonzalez : Ejercicios propuestos
 
-
 # Ejercicio Numero 1 
 '''Cree una clase Punto que reciba las coordenadas (x, y) y un método que indique a qué cuadrante pertenece.'''
 # Definimos la clase con los atributos correspondientes
@@ -8,7 +7,6 @@ class Punto:
     def __init__(self, x, y) -> None: # Recibimos y guardamos las coordenadas 'x' y 'y' del punto
         self.x = x
         self.y = y
-
     def ubicacion(self): # Definimos la funcion que ubicara los puntos en el plano
         if (self.x > 0): # Si 'x' es positivo, se ubicara del lado derecho del plano.
             if (self.y > 0): # Si 'y' es positivo, se ubicara del lado superior del plano
@@ -45,4 +43,4 @@ class Punto:
 
 # Recibimos los valores de la coordenadas por medio de 'input' y los asignamos a los atributos 'x' y 'y'
 p = Punto(float(input('Ingresa la coordenada del eje x : ')),float(input('Ingresa la coordenada del eje y : ')))
-p.ubicacion()
+p.ubicacion() # Accedemos al metodo 'ubicacion()' que nos indica en que cuadrane se ubica nuestro punto
