0f37f67ffd57f3b62219a01701e143842fea6876
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-05-06 18:46:47 -0600

    Part 2 exercises module 8

--- 8-Ficheros/Ejercicios/pacientes.py
+++ 8-Ficheros/Ejercicios/pacientes.py
@@ -16,11 +16,45 @@ class Paciente(_Paciente):
     @property
     def isDiabetic(self):
         '''Regresa 'True' en caso de que el paciente sea diabetico, 'False' en caso contrario'''
-        return True if self.diabetico == 'Si' else False
+        return True if self.diabetico == 'Sí' else False
+    
+    # Agregamos una funcion para imprimir en pantalla los objetos de la clase
+    def __str__(self):
+        return f'Paciente: {self.id} - {self.nombre} {self.apellido}'.ljust(32)+f' | Edad: {self.edad} | Es diabetico?: {self.isDiabetic}'
 
 
-# Probamos el codigo
+# Ejercicio Numero 2 
+'''2.- Dado el archivo “pacientes.txt”, abra y extraiga sus datos. Con ellos, cree instancias de 
+la clase Paciente -definida en el ejercicio anterior- y guárdelos en un diccionario “data”, donde 
+la clave de cada objeto será su correspondiente id. **Considere eliminar los saltos de línea de 
+los valores leídos y convertir los datos id y edad antes de crear el objeto.'''
+# pacientes.txt
+'''
+1;Juan;Perez;42;Sí
+2;Jose;García;15;No
+3;Manuel;Rodriguez;50;Sí
+4;Alicia;Ramirez;12;No
+5;Lucas;Lucyk;25;No
+'''
+# Creamos la ruta a nuestro texto con la informacion de los pacientes
+from os import path
+pacientepath = path.dirname(__file__)+'\pacientes.txt'
 if __name__=='__main__':
-    paciente = Paciente('1525', 'Eduardo', 'Garcia', '25','Si')
-    print(f'Paciente: {paciente.id} - {paciente.nombre} {paciente.apellido}; Edad: {paciente.edad}; Es diabetico? {paciente.isDiabetic}')
-    # Paciente: 1525 - Eduardo Garcia, Edad: 25, Es diabetico? True
+    data = {}
+    # Abrimos el archivo txt
+    with open(pacientepath, encoding="utf-8") as pacientetxt:
+        # Guardamos cada linea leida del archivo en la lista 'pacientesList', eliminando los saltos de linea
+        pacientesList = [paciente.rstrip() for paciente in pacientetxt.readlines()]
+        # Obtenemos los datos individualmente y creamos un objeto con estos datos
+        for _paciente in pacientesList:
+            paciente = Paciente(id=int(_paciente.split(';')[0]), # Convertimos el id obtenido en entero
+                                nombre=_paciente.split(';')[1],
+                                apellido=_paciente.split(';')[2],
+                                edad=int(_paciente.split(';')[3]), # Convertimos la edad obtenida en entero
+                                diabetico=_paciente.split(';')[4]) 
+            # Guardamos los datos de todos los paceintes en un diccionario donde el valor es el id de cada paciente
+            data[str(paciente.id)] = paciente 
+        
+        # Imprimimos en pantalla los datos guardados en nuestro diccionario
+        for k,v in data.items():
+            print(f'Clave = {k}', ' | Valor: ', v)
