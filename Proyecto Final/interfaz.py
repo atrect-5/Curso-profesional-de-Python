a7f590000267a5e8a672730722d4a919e327e723
Alejandro Gonzalez <alex1123.nel@gmail.com>
2024-06-17 21:25:29 -0600

    starting intrfaz calendar

---
+++ Proyecto Final/interfaz.py
@@ -0,0 +1,4 @@
+# Importamos los modulos que usaremos incluyendo el que realizamos anteriormente
+import sys, calendar_events as ce, tkinter as tk, datetime as dt
+from tkinter import ttk, messagebox
+from tkcalendar import Calendar
